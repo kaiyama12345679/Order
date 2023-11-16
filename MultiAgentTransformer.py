66164f80a2f081ff77ef95ea91782719ba0e25ae
kai-yamashita <kai-yama12345679@g.ecc.u-tokyo.ac.jp>
2023-11-16 12:03:51 +0900

    kanso

--- MultiAgentTransformer.py
+++ MultiAgentTransformer.py
@@ -326,7 +326,7 @@ class MultiAgentTransformer(nn.Module):
 
         # Total loss
         self.optimizer.zero_grad()
-        loss = critic_loss + actor_loss + (order_loss)
+        loss = critic_loss + actor_loss + (order_loss / 5)
         loss.backward()
         grad_norm = nn.utils.clip_grad_norm_(
             self.parameters(), max_norm=self.max_grad_norm
