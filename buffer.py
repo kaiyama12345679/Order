8b7ffcba6d425da3d6d8308d67312ae9cc8c4ce4
kai-yamashita <kai-yama12345679@g.ecc.u-tokyo.ac.jp>
2023-12-10 19:44:20 +0900

    bug fix of buffer

--- buffer.py
+++ buffer.py
@@ -58,13 +58,13 @@ class EpisodeBuffer(object):
             (episode_length, self.n_env, self.n_agents, self.obs_dim), device=device
         )
         self.actions = torch.zeros(
-            (episode_length, self.n_env, self.n_agents, 1), device=device
+            (episode_length, self.n_env, self.n_agents, self.action_dim), device=device
         )
         self.action_masks = torch.zeros(
             (episode_length, self.n_env, self.n_agents, self.action_dim), device=device
         )
         self.action_logprobs = torch.zeros(
-            (episode_length, self.n_env, self.n_agents, 1), device=device
+            (episode_length, self.n_env, self.n_agents, self.action_dim), device=device
         )
         self.orders = torch.zeros(
             (episode_length, self.n_env, self.n_agents, 1), device=device
