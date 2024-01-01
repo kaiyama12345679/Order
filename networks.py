6b279047fb4b9b44d00bbab0d7aac25ad34bbf19
kai-yamashita <kai-yama12345679@g.ecc.u-tokyo.ac.jp>
2024-01-01 17:04:27 +0900

    update

--- networks.py
+++ networks.py
@@ -293,7 +293,7 @@ class Pointer(nn.Module):
         return prob
         
 class Transformer_Pointer(nn.Module):
-    C = 0.8
+    C = 10.0
     def __init__(self, n_dim, n_head):
         super().__init__()
 
